71785807a424ba7434a974a5d12b0f4624c3d5b9
Chee-Hyung Yoon <yoonchee@gmail.com>
2013-10-03 16:13:59 +0900

    Add environmental variables.

--- test_facebook.py
+++ test_facebook.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import facebook
@@ -8,14 +9,22 @@ class FacebookAdsAPITest(unittest.TestCase):
 
     def setUp(self):
         try:
+            self.access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
             self.app_id = os.environ['FACEBOOK_APP_ID']
             self.app_secret = os.environ['FACEBOOK_APP_SECRET']
+            self.api = facebook.AdsAPI(
+                self.access_token, self.app_id, self.app_secret)
         except KeyError:
-            raise Exception("FACEBOOK_APP_ID and FACEBOOK_APP_SECRET "
-                            "must be set as environmental variables.")
+            raise Exception("FACEBOOK_ACCESS_TOKEN, FACEBOOK_APP_ID, and "
+                            "FACEBOOK_APP_SECRET must be set as environmental "
+                            "variables.")
 
     def tearDown(self):
         pass
+
+    def test_debug_token(self):
+        response = self.api.debug_token(self.access_token)
+        self.assertEqual(str(response['data']['app_id']), self.app_id)
 
 if __name__ == '__main__':
     unittest.main()
