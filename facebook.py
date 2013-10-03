6790bb8a360ecf786ce27907c99b2ba7578685a5
Chee-Hyung Yoon <yoonchee@gmail.com>
2013-10-03 14:49:08 +0900

    Add testing with Travis.

--- facebook.py
+++ facebook.py
@@ -3,6 +3,7 @@ import hmac
 import json
 import logging
 import urllib
+import urllib2
 
 FACEBOOK_API = 'https://graph.facebook.com'
 
@@ -13,5 +14,29 @@ class AdsAPI(object):
     """A client for the Facebook Ads API."""
     def __init__(self, access_token, app_id, app_secret):
         self.access_token = access_token
-        h = hmac.new(access_token, app_id, app_secret)
+        self.app_id = app_id
+        self.app_secret = app_secret
+        h = hmac.new(access_token, app_secret, hashlib.sha256)
         self.appsecret_proof = h.hexdigest()
+
+    def debug_token(self, token):
+        """Returns debug information about the given token."""
+        path = 'debug_token'
+        args = {
+            'input_token': token,
+            'access_token': '%s|%s' % (self.app_id, self.app_secret)
+        }
+        return self.make_request(path, args)
+
+    def make_request(self, path, args):
+        """Makes a request against the Facebook Ads API endpoint."""
+        url = '%s/%s?%s' % (FACEBOOK_API, path, urllib.urlencode(args))
+        logger.info('Making a GET request at %s' % url)
+        f = urllib2.urlopen(url)
+        return json.load(f)
+
+    def get_adaccount(self, account_id, fields, batch=False):
+        """Returns the fields of a Facebook ad account."""
+        path = 'act_%s' % account_id
+        args = {'fields': fields}
+        return self.make_request(path, args)
---
+++ tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+import facebook
+
+
+class FacebookAdsAPITest(unittest.TestCase):
+    """Tests for Facebook Ads API client."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
