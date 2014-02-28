146cbd2bbaab119e26ea9add153ea112f58f09dc
Kim Taenyon <taenyon.kim@virect.com>
2014-02-28 11:01:50 +0900

    Change log message type.

--- facebook.py
+++ facebook.py
@@ -133,7 +133,7 @@ class AdsAPI(object):
             for idx, val in enumerate(data):
                 # Workaround code for facebook api server error
                 if val['code'] == 500:
-                    logger.error("Facebook api server has some problem.")
+                    logger.info("Facebook api server has some problem.")
                     val['body'] = '{"error": {"code": 1, "message": "An unknown error occurred", "type": "UnknownError"}}'
                 data[idx] = json.loads(val['body'])
             return data
