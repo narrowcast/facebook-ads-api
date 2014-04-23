44f0aa91abe7604f75ea995dde26de7b7d99ab80
Aron Griffis <aron@arongriffis.com>
2014-04-23 17:25:10 -0400

    Add support for max_bid in create_adgroup

--- facebook.py
+++ facebook.py
@@ -495,7 +495,7 @@ class AdsAPI(object):
         return self.make_request(path, 'POST', args, batch=batch)
 
     def create_adgroup(self, account_id, name, bid_type, bid_info, campaign_id,
-                       creative_id, targeting, conversion_specs=None,
+                       creative_id, targeting, max_bid=None, conversion_specs=None,
                        tracking_specs=None, view_tags=None, batch=False):
         """Creates an adgroup in the given ad camapaign with the given spec."""
         path = 'act_%s/adgroups' % account_id
@@ -507,6 +507,10 @@ class AdsAPI(object):
             'creative': json.dumps({'creative_id': creative_id}),
             'targeting': targeting,
         }
+        if max_bid:
+            assert bid_type == 'CPM', 'can only use max_bid with CPM bidding'
+            args['max_bid'] = max_bid
+            del args['bid_info']  # get rid of bid_info
         if conversion_specs:
             args['conversion_specs'] = json.dumps(conversion_specs)
         if tracking_specs:
