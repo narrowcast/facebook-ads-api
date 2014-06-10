96e1ded07a80327bdc2aa2d569d08bbb7bc5e4fd
Chee-Hyung Yoon <yoonchee@gmail.com>
2014-06-10 10:39:33 +0900

    Bump up the version after incorporating some patches.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class TestCommand(Command):
 
 setup(
     name='facebook-ads-api',
-    version='0.1.32',
+    version='0.1.33',
     author='Chee-Hyung Yoon',
     author_email='yoonchee@gmail.com',
     py_modules=['facebook', ],
