64bdf615bb25e3731b9dd0806a33d7639bddc338
Chee-Hyung Yoon <yoonchee@gmail.com>
2014-08-29 11:36:02 +0900

    Bump up version for adding a feature.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class TestCommand(Command):
 
 setup(
     name='facebook-ads-api',
-    version='0.1.39',
+    version='0.1.40',
     author='Chee-Hyung Yoon',
     author_email='yoonchee@gmail.com',
     py_modules=['facebook', ],
