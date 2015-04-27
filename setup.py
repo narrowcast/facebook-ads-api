c8ecd0e0ae21bd27a67281cf3e06f04ffcb8a70b
Chee-Hyung Yoon <yoonchee@gmail.com>
2015-04-27 08:27:09 -0700

    Bump up version to integrate paging feature.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class TestCommand(Command):
 
 setup(
     name='facebook-ads-api',
-    version='0.2.0',
+    version='0.2.1',
     author='Chee-Hyung Yoon',
     author_email='yoonchee@gmail.com',
     py_modules=['facebook', ],
